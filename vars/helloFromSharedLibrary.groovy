de601f014ddf7640f3fa64c27cb3a01c4b73da54
singh.gaurav <singh.gaurav@cardinalhealth.com>
2018-08-28 07:18:18 -0400

    updating parameter value

--- vars/helloFromSharedLibrary.groovy
+++ vars/helloFromSharedLibrary.groovy
@@ -1,7 +1,7 @@
-def call(){
+def call(String workspace ){
  
   sh """
-    echo 'Greetings from HelloFromShared_library from HelloFile'
+    echo 'Greetings from HelloFromShared_library from HelloFile and this is using method argument: ${workspace}'
   """
 }
 
