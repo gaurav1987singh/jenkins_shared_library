226a158101fef256a77d6a7cb3ac9b67e7d7ee75
singh.gaurav <singh.gaurav@cardinalhealth.com>
2018-08-29 07:36:53 -0400

    adding file name

---
+++ vars/loadProperties.groovy
@@ -0,0 +1,11 @@
+def call() {
+
+  def scriptsPath = ''
+ 
+  scriptsPath = '/home/585933/rNd_work/gitHUb/jenkins_shared_library'
+
+  
+  def properties = readProperties file:"${scriptsPath}/Jenkins/test_builds.properties"
+  
+}
+
