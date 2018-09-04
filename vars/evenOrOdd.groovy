349c16616a5b185f59df74ef0a85ecebf9c79667
singh.gaurav <singh.gaurav@cardinalhealth.com>
2018-09-04 10:28:07 -0400

    adding even_odd.groovy

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -1,25 +1,10 @@
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
+    
             echo "The build number is even"
           }
-        }
-      }
-    }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
+ else {
+    
             echo "The build number is odd"
-          }
-        }
-      }
-    }
-  }
-}
+       }
+ }
