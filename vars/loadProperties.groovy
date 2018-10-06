18e98a8cff6e8c116a3ffe85cb0ab93eac5b7406
gaurav1987singh <mail.gauravsinghnegi@gmail.com>
2018-10-06 20:47:24 +0530

    changing file path

--- vars/loadProperties.groovy
+++ vars/loadProperties.groovy
@@ -2,10 +2,10 @@ def call() {
 
   def scriptsPath = ''
  
-  scriptsPath = '/home/585933/rNd_work/gitHUb/jenkins_shared_library'
+  scriptsPath = '../Jenkins/test_builds.properties'
 
   
-  def properties = readProperties file:"${scriptsPath}/Jenkins/test_builds.properties"
+  def properties = readProperties file:"$scriptPath"
   return properties
 }
 
